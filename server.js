aa198301072093d7d2c89f333139b21ea65da84f
Your Name <you@example.com>
2020-09-21 17:57:21 +0300

    terul

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const fs = require('fs');
+const Url = require('url-parse');
 
 http.createServer((Request, Response) => {
+    if(Request.url === '/favicon.ico') return;
+    
+    const url = new Url(Request.url, true);
+    
+    console.log(url.query.name);
     const file = fs.readFileSync('index.html', { encoding: "utf-8"} )
 
 
@@ -9,3 +15,5 @@ Response.writeHead(200, {'Content-Type': 'text/html'});
 Response.write(file);
 Response.end();
 }).listen(1111);
+
+console.log("Listenning...");
