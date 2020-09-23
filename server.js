f6196f5e020976bc35adee4ccecfa5989579b238
Your Name <you@example.com>
2020-09-23 13:34:07 +0300

    new

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ http.createServer((Request, Response) => {
     
     const url = new Url(Request.url, true);
     
-    console.log(url.query.name);
-    const file = fs.readFileSync('index.html', { encoding: "utf-8"} )
+    const name = url.query.name;
+    const age = url.query.age;
 
+    const person = {
+      name,
+      age,
+    };
+
+    fs.writeFileSync('db.json', JSON.stringify([person]));
+
+    let file = fs.readFileSync('index.html', { encoding: "utf-8"} )
+    file = file.replace(/%name%/g, name);
 
 Response.writeHead(200, {'Content-Type': 'text/html'});
 Response.write(file);
